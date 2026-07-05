874934d31dc951199b2b83efce1c5b78374e343e
Ilia Sharin <isharin@gmail.com>
2026-07-04 23:20:21 -0400

    Fix textinput include path for CI

--- libs/compat/mui/textinput_mcc.h
+++ libs/compat/mui/textinput_mcc.h
@@ -1,5 +1,5 @@
 /* Case-sensitive include wrapper: mui/textinput_mcc.h -> MUI/Textinput_mcc.h */
 #ifndef COMPAT_TEXTINPUT_MCC_H
 #define COMPAT_TEXTINPUT_MCC_H
-#include "../../MCC_Textinput/Developer/C/Include/MUI/Textinput_mcc.h"
+#include <MUI/Textinput_mcc.h>
 #endif
